2852f16a6d763b28ed548a67505affd21b417a66
nerod10 <65760965+nerod10@users.noreply.github.com>
2020-07-29 14:56:59 +0300

    Initial commit

---
+++ src/ListOffers.js
@@ -0,0 +1,5 @@
+import React from "react";
+function ListOffers() {
+  return "12345556".split("").map(x => <h1>{"bla " + x}</h1>);
+}
+export default ListOffers;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ListOffers from "./ListOffers";
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <div>
+    <h1>dfsf</h1>
+    <ListOffers />
+    <App />
+  </div>,
+  rootElement
+);
